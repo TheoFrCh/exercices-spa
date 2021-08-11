dc81c0255b5759e3fdb464a97cde96ab44c689f8
rdhox <admin@rdhox.io>
2021-08-11 10:21:35 +0200

    fix mistake

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import * as ε from './tools/ε';
-import Header from './components/Header';
+import App from './components/App';
 
 
 // Point d'entré de votre site.
 
-ε.start(Header, document.getElementById('root'));
+ε.start(App, document.getElementById('root'));
