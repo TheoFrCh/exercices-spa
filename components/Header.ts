a1b690b54a613a1916c19a171d83de7a3f748082
Theo <theo.chiron26@gmail.fr>
2021-09-03 09:29:15 +0200

    first commit

--- components/Header.ts
+++ components/Header.ts
@@ -5,23 +5,35 @@ import Title from './Title';
 
 // We want to render html like: 
 
-  //     <div class="w-full flex justify-center items-center border-2 bg-gray-300">
-  //       <Title type="h1" >HELLO WORLD</Title>
-  //     </div>
+//     <div class="w-full flex justify-center items-center border-2 bg-gray-300">
+//       <Title type="h1" >HELLO WORLD</Title>
+//     </div>
 
 class Header extends ε.Component {
 
-  render() {
-    return ε.createElement(
-        'div',
-        {
-          class: "w-full flex justify-center items-center border-2 bg-gray-300",
-        },
-        [
-          ε.createElement(Title,{type: "h1"}, ["Hello world"])
-        ]
-      )
-  }
+    render() {
+        return ε.createElement(
+            'div',
+            {
+                class: "w-full flex flex-col justify-center items-center border-2 bg-gray-300",
+            },
+            [
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+                ε.createElement(Title, {type: "h1"}, ["Hello world"]),
+
+
+            ]
+        )
+    }
 }
 
 export default Header;
--- tools/ε/index.ts
+++ tools/ε/index.ts
@@ -1,58 +1,83 @@
-import { Element } from './frameworkTypes';
+import {Element} from './frameworkTypes';
 
 // parent class for components
 abstract class Component {
-  abstract render(): Element
+    abstract render(): Element
 }
 
 function createElement(name, attributes, children) {
-  return {
-    name,
-    attributes,
-    children
-  };
+    return {
+        name,
+        attributes,
+        children
+    };
 };
 
+
+function getHTMLforElement(element) {
+    let HTMLtoReturn;
+    if (typeof element === 'string') {
+        HTMLtoReturn = element;
+    } else {
+        const tag = document.createElement(element.name)
+        for (const key in element.attributes) {
+            tag.setAttribute(key, element.attributes[key])
+        }
+        element.children.forEach(child => {
+            const res = getHTMLforElement(child)
+            if (typeof res === "string") {
+                tag.innerHTML = res
+            } else {
+                tag.appendChild(res)
+            }
+        });
+        HTMLtoReturn = tag;
+    }
+
+
+    return HTMLtoReturn;
+}
+
 function elementsToHTML(virtualDom) {
-  // TODO
+    return getHTMLforElement(virtualDom)
 }
 
 function getElement(element) {
-  let elementToReturn;
-  if (typeof element === 'string') {
-    elementToReturn = element;
-  } else if(typeof element.name === 'string') {
-    const childrenCol = [];
-    element.children.forEach(e => {
-      childrenCol.push(getElement(e));
-    });
-    element.children = childrenCol;
-    elementToReturn = element;
-  } else {
-    const instance = new element.name();
-    const render = instance.render();
-    elementToReturn = getElement(render);
-  }
+    let elementToReturn;
+    if (typeof element === 'string') {
+        elementToReturn = element;
+    } else if (typeof element.name === 'string') {
+        const childrenCol = [];
+        element.children.forEach(e => {
+            childrenCol.push(getElement(e));
+        });
+        element.children = childrenCol;
+        elementToReturn = element;
+    } else {
+        const instance = new element.name();
+        const render = instance.render();
+        elementToReturn = getElement(render);
+    }
 
-  return elementToReturn;
+    return elementToReturn;
 }
-
 
 
 function getVirtualDom(element) {
-  return getElement(element);
+    return getElement(element);
 }
 
 function start(rootComponent, rootHtml: HTMLElement): void {
-  const rootInstance = new rootComponent();
-  const rootRender = rootInstance.render();
-  const virtualDom = getVirtualDom(rootRender);
-  const site = elementsToHTML(virtualDom);
-  console.log(site);
+    const rootInstance = new rootComponent();
+    const rootRender = rootInstance.render();
+    const virtualDom = getVirtualDom(rootRender);
+    const site = elementsToHTML(virtualDom);
+    rootHtml.appendChild(site)
+    console.log(virtualDom);
 }
 
 export {
-  Component,
-  start,
-  createElement
+    Component,
+    start,
+    createElement
 };
